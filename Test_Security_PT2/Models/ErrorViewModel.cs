d8c6715565c9b5abe510c2034a3f9ce851acf1a3
Trần Quang <quangtranhhpt1@gmail.com>
2023-06-25 13:59:38 +0700

    Add project files.

---
+++ Test_Security_PT2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_Security_PT2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
